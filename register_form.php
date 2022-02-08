e4956ea081878e9f2c3025e6cda25730fe003c4f
C.V.Anish <91870659+C-V-Anish@users.noreply.github.com>
2022-02-09 01:11:08 +0530

    Add files via upload

---
+++ register_form.php
@@ -0,0 +1,25 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Registration Form</h1>
+    <form action="register_submit.php" method="post">
+        Full Name :
+        <input type="text" name="name">
+        <br>
+        Email Address : 
+        <input type="email" name="email">
+        <br>
+        Set Password : 
+        <input type="password" name="password1">
+        Confirm Password : 
+        <input type="password" name="password1">
+        <br>
+        <input type="submit">
+    </form>
+</body>
+</html>
